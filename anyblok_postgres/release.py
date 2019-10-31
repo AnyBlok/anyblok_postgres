6e76f8eec1a0be4fe2179bd085e65998b20e05c6
jssuzanne <js.suzanne@gmail.com>
2019-10-31 16:48:34 +0100

    0.2.2

--- anyblok_postgres/release.py
+++ anyblok_postgres/release.py
@@ -6,4 +6,4 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
-version = '0.2.1'  # pragma: no cover
+version = '0.2.2'  # pragma: no cover
